7d9bc87a1f8ae9f312175a372510ee729712a358
Zulfira21 <zulfira.seitkulova.kk@gmail.com>
2021-11-05 23:45:53 +0600

    homework 31

--- main/models.py
+++ main/models.py
@@ -6,3 +6,10 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class ToMeet(models.Model):
+    person = models.CharField(max_length=50)
+    phone_Number = models.CharField(max_length=16,unique=True)
+    date_of_meeting = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
